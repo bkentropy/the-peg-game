55cdb9e878c50c4263a8d105df391e52bf87f042
Brian Kustra <bkustrakb@gmail.com>
2016-06-11 14:02:53 -0500

    Initialize repo, added require statement

---
+++ src/peg_game/core.clj
@@ -0,0 +1,12 @@
+(ns peg-game.core
+  (require [clojure.set :as set])
+  (:gen-class))
+
+(declare successful-move prompt-move game-over query-rows)
+
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
